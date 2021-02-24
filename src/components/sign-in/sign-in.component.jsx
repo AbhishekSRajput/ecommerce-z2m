da6ee647c4066000bdde4a9a218cd119c573f2ee
ApcGamer <abhisingh3@hotmail.com>
2021-02-24 20:29:26 +0530

    integrated firebase in sign in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -8,60 +8,60 @@ import { signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			email: '',
-			password: '',
-		};
-	}
+    this.state = {
+      email: '',
+      password: ''
+    };
+  }
 
-	handleSubmit = (event) => {
-		event.preventDefault();
+  handleSubmit = event => {
+    event.preventDefault();
 
-		this.setState({ email: '', password: '' });
-	};
+    this.setState({ email: '', password: '' });
+  };
 
-	handleChange = (event) => {
-		const { value, name } = event.target;
+  handleChange = event => {
+    const { value, name } = event.target;
 
-		this.setState({ [name]: value });
-	};
+    this.setState({ [name]: value });
+  };
 
-	render() {
-		return (
-			<div className="sign-in">
-				<h2>I already have an account</h2>
-				<span>Sign in with your email and password</span>
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
-				<form onSubmit={this.handleSubmit}>
-					<FormInput
-						name="email"
-						type="email"
-						handleChange={this.handleChange}
-						value={this.state.email}
-						label="email"
-						required
-					/>
-					<FormInput
-						name="password"
-						type="password"
-						value={this.state.password}
-						handleChange={this.handleChange}
-						label="password"
-						required
-					/>
-					<div className="buttons">
-						<CustomButton type="submit"> SIGN IN </CustomButton>
-						<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-							SIGN IN WITH GOOGLE
-						</CustomButton>
-					</div>
-				</form>
-			</div>
-		);
-	}
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: 'AIzaSyCx0rmAXs6qn6pjiJbe_7fCQpehf4Io3i4',
-	authDomain: 'e-commerce-z2m.firebaseapp.com',
-	databaseURL: 'https://e-commerce-z2m-default-rtdb.firebaseio.com',
-	projectId: 'e-commerce-z2m',
-	storageBucket: 'e-commerce-z2m.appspot.com',
-	messagingSenderId: '237769603126',
-	appId: '1:237769603126:web:7e8d69ce82d712ae608097',
-	measurementId: 'G-5E6CTKHV7T',
+	apiKey: 'AIzaSyAx0e84_qgnobTARDmWfuFLAUXcQbwov8o',
+	authDomain: 'crud-e5ced.firebaseapp.com',
+	databaseURL: 'https://crud-e5ced-default-rtdb.firebaseio.com',
+	projectId: 'crud-e5ced',
+	storageBucket: 'crud-e5ced.appspot.com',
+	messagingSenderId: '196637442551',
+	appId: '1:196637442551:web:a7b2e806a4f9d253916ef2',
+	measurementId: 'G-VY4242R0MP',
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -20,7 +20,9 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
+
 provider.setCustomParameters({ prompt: 'select_account' });
+
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
