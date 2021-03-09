32044636f292f7a16cff8ef8cdcc44c0b4cf4bf7
ApcGamer <abhisingh3@hotmail.com>
2021-03-10 00:50:13 +0530

    added remove item functionality and remove item utility in redux

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+
+import {
+	addItem,
+	clearItemFromCart,
+	removeItem,
+} from '../../redux/cart/cart.actions';
+
 //style
 import './checkout-item.style.scss';
 
-const CheckoutItemComponent = ({ cartItem, clearItem }) => {
+const CheckoutItemComponent = ({
+	cartItem,
+	clearItem,
+	addItem,
+	removeItem,
+}) => {
 	const { imageUrl, name, quantity, price } = cartItem;
 	return (
 		<div className="checkout-item">
@@ -13,7 +24,17 @@ const CheckoutItemComponent = ({ cartItem, clearItem }) => {
 				<img src={imageUrl} alt="item" />
 			</div>
 			<span className="name">{name}</span>
-			<span className="quantity">{quantity}</span>
+			<span className="quantity">
+				<div className="arrow" onClick={() => removeItem(cartItem)}>
+					{' '}
+					&#10094;
+				</div>
+				<span className="value">{quantity}</span>
+				<div className="arrow" onClick={() => addItem(cartItem)}>
+					{' '}
+					&#10095;{' '}
+				</div>
+			</span>
 			<span className="price">{price}</span>
 			<div className="remove-button" onClick={() => clearItem(cartItem)}>
 				{' '}
@@ -24,6 +45,8 @@ const CheckoutItemComponent = ({ cartItem, clearItem }) => {
 };
 const mapDispatchToProps = (dispatch) => ({
 	clearItem: (item) => dispatch(clearItemFromCart(item)),
+	addItem: (item) => dispatch(addItem(item)),
+	removeItem: (item) => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItemComponent);
